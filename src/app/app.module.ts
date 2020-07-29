cf848d670fa7d42d4938979b60d16749e944c430
lbanozic <lubanozic@gmail.com>
2020-07-29 10:35:20 +0200

    Add device battery level and signal strength indicator in popover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PortalModule } from '@angular/cdk/portal';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +8,7 @@ import { DeviceListComponent } from './device-list/device-list.component';
 import { DeviceComponent } from './device/device.component';
 import { IconInfoComponent } from './icons/icon-info/icon-info.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PopoverComponent } from './popover/popover.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +17,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     DeviceComponent,
     DeviceListComponent,
     IconInfoComponent,
+    PopoverComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, PortalModule, OverlayModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Device } from '../device';
+import { PopoverComponent } from '../popover/popover.component';
 
 @Component({
   selector: 'dm-device',
@@ -9,7 +10,38 @@ import { Device } from '../device';
 export class DeviceComponent implements OnInit {
   @Input() device: Device;
 
+  @ViewChild(PopoverComponent)
+  public popover: PopoverComponent;
+
+  readonly colorRed: string = '#dc3545';
+  readonly colorBlack: string = '#000';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  get iconColor(): string {
+    if (this.device.batteryLevel < 50 || this.device.signalStrength < 5) {
+      return this.colorRed;
+    }
+    return this.colorBlack;
+  }
+
+  get deviceBatteryLevelTextColor(): string {
+    if (this.device.batteryLevel < 50) {
+      return this.colorRed;
+    }
+    return this.colorBlack;
+  }
+
+  get signalStrengthTextColor(): string {
+    if (this.device.signalStrength < 5) {
+      return this.colorRed;
+    }
+    return this.colorBlack;
+  }
+
+  showPopover() {
+    this.popover.show();
+  }
 }
