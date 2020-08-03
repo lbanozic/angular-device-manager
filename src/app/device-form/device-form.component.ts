235513382c2a13973abe13973245818743fcd229
lbanozic <lubanozic@gmail.com>
2020-08-03 20:32:48 +0200

    Add min-max validation and validation messages

--- src/app/device-form/device-form.component.ts
+++ src/app/device-form/device-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'dm-device-form',
@@ -7,19 +13,51 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./device-form.component.css'],
 })
 export class DeviceFormComponent implements OnInit {
-  deviceForm = new FormGroup({
-    devui: new FormControl('', [
-      Validators.required,
-      Validators.pattern('#+([a-fA-F0-9]{6})$'),
-    ]),
-    min: new FormControl(''),
-    max: new FormControl(''),
-    name: new FormControl('', Validators.required),
-  });
+  deviceForm: FormGroup;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.deviceForm = new FormGroup(
+      {
+        devui: new FormControl('', [
+          Validators.required,
+          Validators.pattern('#+([a-fA-F0-9]{6})$'),
+        ]),
+        min: new FormControl(''),
+        max: new FormControl(''),
+        name: new FormControl('', Validators.required),
+      },
+      this.minMaxValidator
+    );
+  }
+
+  get devui() {
+    return this.deviceForm.get('devui');
+  }
+
+  get min() {
+    return this.deviceForm.get('min');
+  }
+
+  get max() {
+    return this.deviceForm.get('max');
+  }
+
+  get name() {
+    return this.deviceForm.get('name');
+  }
+
+  get minMaxValidator(): ValidatorFn {
+    return (control: FormGroup): ValidationErrors | null => {
+      const minValue = control.get('min').value;
+      const maxValue = control.get('max').value;
+
+      return minValue && maxValue && minValue > maxValue
+        ? { minMaxInvalid: true }
+        : null;
+    };
+  }
 
   onSubmit() {
     console.info(this.deviceForm.value);
