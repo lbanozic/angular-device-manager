141acd97aca96114e91f7c286f0754b9d194d5b0
lbanozic <lubanozic@gmail.com>
2020-08-09 20:10:09 +0200

    Rename device-mock to device-generator

--- src/app/device.service.ts
+++ src/app/device.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Device, DeviceStatus } from './device';
-import { DEVICES } from './device-mock';
+import { DEVICES } from './device-generator';
 
 @Injectable({
   providedIn: 'root',
