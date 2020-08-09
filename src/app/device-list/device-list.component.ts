4e4b0a36d38e07fd0f7ddf0e048dbb8d9c534c74
lbanozic <lubanozic@gmail.com>
2020-08-09 21:59:14 +0200

    Add trackBy and async rendering with loading indicator to device list

--- src/app/device-list/device-list.component.ts
+++ src/app/device-list/device-list.component.ts
@@ -10,13 +10,22 @@ import { DeviceService } from '../device.service';
 export class DeviceListComponent implements OnInit {
   devices: Device[];
 
+  isLoading = false;
+
   constructor(private deviceService: DeviceService) {}
 
   ngOnInit(): void {
-    this.deviceService.devices$.subscribe(
-      (deviceList) =>
-        (this.devices = this.deviceService.getSortedDevices(deviceList))
-    );
+    this.deviceService.devices$.subscribe((deviceList) => {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.devices = this.deviceService.getSortedDevices(deviceList);
+        this.isLoading = false;
+      });
+    });
     this.deviceService.getDevices();
+  }
+
+  trackByItems(index: number, device: Device): string {
+    return device.devui;
   }
 }
